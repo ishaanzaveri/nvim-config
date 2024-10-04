625d1ce265220de6272cdd914b400f5e2151a25f
ishaan <ishaan@Debain.myguest.virtualbox.org>
2024-10-04 13:33:32 -0500

    Initial commit for nvim config

---
+++ lua/ishaan/packer.lua
@@ -0,0 +1,25 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use('nvim-treesitter/nvim-treesitter',{run = ':TSUpdate'})
+    use('nvim-treesitter/playground')
+    use('mbbill/undotree')
+    use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+    use({'neovim/nvim-lspconfig'})
+    use({'hrsh7th/nvim-cmp'})
+    use({'hrsh7th/cmp-nvim-lsp'})
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+end)
